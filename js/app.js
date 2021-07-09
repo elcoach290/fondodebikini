59a890b49551c075a01157a5ade1792de656a9db
elcoach290 <martin.rios.reynoso@gmail.com>
2021-07-08 20:28:04 -0500

    archivos subidos desde del navegador

---
+++ js/app.js
@@ -0,0 +1,66 @@
+//alert("Bienvenidos a mi sitio Web");
+//alert("Espero disfruten su estancia");
+
+// let ciudad = "Santiago";
+// let pais = "Chile";
+// let edad = 14;
+// let usuario = "Peter Castle";
+// let sueldo = 5;
+// let colorcito = "tomato";
+// let cabecera = document.querySelector("header");
+// let botonTomatias = document.querySelector("#tomatias");
+
+
+
+// Alexa
+
+// let piedepagina = document.querySelector("footer");
+
+
+// botonTomatias.addEventListener("click", function(){
+// 	cabecera.style.backgroundColor = colorcito;
+// 	piedepagina.style.background= colorcito;
+// });
+
+
+// Omar Alcala
+
+//let pie = document.querySelector("footer");
+//pie.style.backgroundColor = colorcito;
+
+
+//alert("hola estamos en "+ciudad+"-"+pais+" mi nombre es "+usuario+" y te has ganado S/"+sueldo+" solo por visitarnos");
+
+// Fernando V.
+//alert( "Estoy en "+ciudad+", necesito "+sueldo+" podrian prestarme?");
+
+// Paul G.
+//alert("Hola, me llamo "+usuario+" y NO tengo "+sueldo+" años. Tengo "+edad+" y contando. ");
+
+// Omar Alcala
+
+//alert("Hola en "+ciudad+" keiko sigue rezando para ganar la presidencia del "+pais );
+
+// Medaly
+
+//alert("Hola mi nombre es "+usuario+" y me gustaria visitar "+ciudad+" muy pronto");
+
+// Dafne
+
+//alert("Soy Dafne de "+ciudad+", quiero"+sueldo+" para viajar");
+
+// Raphael
+
+//alert("Hola mi nombre es "+usuario+" no tengo "+dinero+" ni trabajo");
+
+// Geraldine
+
+//alert("¡Hola! Me llamo "+usuario+" estoy perdida en "+ciudad+" ¿Me prestan S/"+sueldo+" para regresar a mi casita? Gracias");
+
+// Christian
+
+//alert( "Soy de "+ciudad+" y necesito S/. "+sueldo+"para pagar una extorsión :c");
+
+$(".mr-slideshow").slick({ autoplay: true, dots: true});
+
+
